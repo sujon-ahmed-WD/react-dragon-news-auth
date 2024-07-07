45c4881187d6b56d378e218c14be0ad5445572cc
sujon-ahmed-WD <sujon.mrist@gmail.com>
2024-07-07 20:16:17 +0600

    added login route

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+ 
+const Login = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h1>This is Login pages</h1>
+        </div>
+    );
+};
+
+export default Login;
